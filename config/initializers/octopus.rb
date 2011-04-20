b645b0fd9bc336fff0b1d492aa56f58927eb9ca9
Matthew Soldo <matt@soldo.org>
2011-04-19 23:03:57 -0700

    manual shard definition

--- config/initializers/octopus.rb
+++ config/initializers/octopus.rb
@@ -1,6 +1,7 @@
 ## hash of the shards we will create
 ## the all_shards key contains an embedded hash used to group the shards
 # shards = {:all_shards => {}}
+# require 'octopus'
 shards = {}
 ENV.each_with_index do |env_variable, index|
   # Test if its a Heroku PostgreSQL URL
@@ -22,10 +23,22 @@ end
 #   config.shards = shards
 # end
 
-Octopus.setup do |config|
-  config.shards = {
-    :white_shard => {:adapter => "postgresql", :database => "dj21lsmiewecscn", :username => "uf3ljol7ufl6wji", :password => "p7fz8kjndmo8y4ia3omwk8mre3", :host => "ec2-50-19-122-152.compute-1.amazonaws.com"},
-    :yellow_shard => {:adapter => "postgresql", :database => "dc37wq7vnsskr83", :username => "uy2nwf64mvc59tm", :password => "pa93p1b6acd4fcrcmdbt5qbxox", :host => "ec2-50-19-122-155.compute-1.amazonaws.com"}
-  }
-  config.environments = [:production]
-end
+# Octopus.setup do |config|
+#   config.shards = {
+#     :white_shard => {
+#       :adapter => "postgresql", 
+#       :database => "dj21lsmiewecscn", 
+#       :username => "uf3ljol7ufl6wji", 
+#       :password => "p7fz8kjndmo8y4ia3omwk8mre3", 
+#       :host => "ec2-50-19-122-152.compute-1.amazonaws.com"
+#     },
+#     :yellow_shard => {
+#       :adapter => "postgresql", 
+#       :database => "dc37wq7vnsskr83", 
+#       :username => "uy2nwf64mvc59tm", 
+#       :password => "pa93p1b6acd4fcrcmdbt5qbxox", 
+#       :host => "ec2-50-19-122-155.compute-1.amazonaws.com"
+#     }
+#   }
+#   config.environments = [:production]
+# end
