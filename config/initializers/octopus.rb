a8f2cc1e3e38b9ef5e664c8a2fc11782d738e5a9
Matthew Soldo <matt@soldo.org>
2011-04-19 22:42:34 -0700

    manually set shards

--- config/initializers/octopus.rb
+++ config/initializers/octopus.rb
@@ -17,7 +17,15 @@ ENV.each_with_index do |env_variable, index|
   end 
 end
  
+# Octopus.setup do |config|
+#   config.environments = [:production]
+#   config.shards = shards
+# end
+
 Octopus.setup do |config|
+  config.shards = {
+    :white_shard => {:adapter => "postgresql", :database => "dj21lsmiewecscn", :username => "uf3ljol7ufl6wji", :password => "p7fz8kjndmo8y4ia3omwk8mre3", :host => "ec2-50-19-122-152.compute-1.amazonaws.com"},
+    :yellow_shard => {:adapter => "postgresql", :database => "dc37wq7vnsskr83", :username => "uy2nwf64mvc59tm", :password => "pa93p1b6acd4fcrcmdbt5qbxox", :host => "ec2-50-19-122-155.compute-1.amazonaws.com"}
+  }
   config.environments = [:production]
-  config.shards = shards
 end
